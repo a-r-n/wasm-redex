b69509c9e47ce87aeaf21907db07cda6d4eab618
Aaron Nelson <arn@arn.xyz>
2021-02-23 15:27:21 -0600

    Add early WASM machine core

--- wasm.rkt
+++ wasm.rkt
@@ -23,153 +23,144 @@
        (traces s (l p))
        (test+validate s f ex (l p) r))]))
 
-;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;
-
-(define-language WebAssembly
-  (t ::= i32 i64 f32 f64)
-  (f ::= (func i (param i t) ... e ...))
-  (m ::= (module f ...))
-  (e ::= v (select e e x) (t binop e e) (call i e ...) (get-local i))
-  (v ::= (t const c))
-  (op ::= binop relop)
-  (binop ::= add)
-  (relop ::= eq)
+(define-language WASM
   (c ::= real)
+  (e ::= binop (const c) (call i) trap debug-inst
+     (get-local i) (param i))
+  (binop ::= add sub mul div)
+  (f ::= (func i e ...))
+  (m ::= (module f ...))
   (i ::= variable-not-otherwise-mentioned)
 
-  ; Special "function expression", for expressions which only make sense in the context of a function
-  (fe ::= e (param i t))
+  ; Debugging statement, should never show up in a valid program
+  (debug-inst ::= (debug any ...)))
 
-  (init ::= (m e)) ; Helper to load module and execute something
+(define-extended-language WASM-eval WASM
+  (op ::= binop)
+  (init ::= (m e ...))
   
-  (hashmap ::= mt-h (i v hashmap))
-  (scope ::= mt-s (hashmap (v ...) scope))
-  (table ::= mt-t (i f table))
-  (κ ::= mt-k
-     (function-expressions fe ... κ)
-     (module-stack e (f ...) κ)
-     (prim t (v ... op) (e ...) κ)
-     (call-stack i (v ...) (e ...) κ)))
+  (s ::= (s-func s-table s-mem))
+  (s-func ::= mt-s-func (i f s-func))
+  (s-table ::= mt-s-table)
+  (s-mem ::= mt-s-mem)
+  
+  (L ::= mt-context (local (e ...) L))
+  (local ::= mt-local (i v local))
+  (v ::= (const c)))
 
-(define-metafunction WebAssembly
+;;;;;; METAFUNCTIONS ;;;;;;
+
+;; Delta
+
+(define-metafunction WASM-eval
   delta : op v ... -> v
-  [(delta add (t const c_1) (t const c_2)) (t const ,(+ (term c_1) (term c_2)))])
+  [(delta add (const c_0) (const c_1)) (const ,(+ (term c_0) (term c_1)))])
 
-; This and function-get could be abstracted to the same metafunction?
-(define-metafunction WebAssembly
-  hashmap-get : hashmap i -> v
-  [(hashmap-get (i_map v hashmap_next) i_arg) ,(if (equal? (term i_arg) (term i_map))
-                                                   (term v)
-                                                   (term (hash-get hashmap_next i_arg)))])
+;; Get and add functions
 
-(define-metafunction WebAssembly
-  hashmap-add : hashmap i v -> hashmap
-  [(hashmap-add hashmap i v) (i v hashmap)])
+(define-metafunction WASM-eval
+  function-add : s f -> s
+  [(function-add (s-func s-table s-mem) (func i e ...)) ((i (func i e ...) s-func) s-table s-mem)])
 
-(define-metafunction WebAssembly
-  function-get : table i -> f
-  [(function-get (i_table f table_next) i_arg) ,(if (equal? (term i_arg) (term i_table))
-                                                    (term f)
-                                                    (term (function-get table_next i_arg)))])
-(define-metafunction WebAssembly
-  function-add : table f -> table
-  [(function-add table (func i_1 (param i_2 t) ... e ...)) (i_1 (func i_1 (param i_2 t) ... e ...) table)])
+(define-metafunction WASM-eval
+  function-get-internal : s-func i -> f
+  [(function-get-internal (i_func f s-func) i_arg) ,(if (equal? (term i_arg) (term i_func))
+                                                        (term f)
+                                                        (term (function-get-internal
+                                                               s-func i_arg)))]
+  [(function-get-internal mt-s-func i) (func DEBUG-BAD (debug "function not defined" i))])
+
+(define-metafunction WASM-eval
+  function-get : s i -> f
+  [(function-get (s-func s-table s-mem) i) (function-get-internal s-func i)])
+
+;; Local variables
+
+(define-metafunction WASM-eval
+  local-set-internal : local i v -> local
+  [(local-set-internal (i_local v_local local) i_arg v_arg)
+   ,(if (equal? (term i_local) (term i_arg))
+        (term (i_local v_arg local))
+        (term (i_local v_arg (local-set-internal local i_arg v_arg))))]
+  [(local-set-internal mt-local i v) (i v mt-local)])
+
+(define-metafunction WASM-eval
+  local-get-internal : local i -> v
+  [(local-get-internal (i_local v_local local) i_arg)
+   ,(if (equal? (term i_local) (term i_arg))
+        (term v_local)
+        (term (local-get-internal local i_arg)))])
+
+(define-metafunction WASM-eval
+  local-set : L i v -> L
+  [(local-set (local (e ...) L) i v) ((local-set-internal local i v) (e ...) L)])
+
+(define-metafunction WASM-eval
+  local-get : L i -> v
+  [(local-get (local (e ...) L) i) (local-get-internal local i)])
+  
+
+;; MACHINE
+;; ((values expressions) (context) (store))
+
+;;;;;;; REDUCTION ;;;;;;
 
 (define ->
   (reduction-relation
-   WebAssembly
+   WASM-eval
 
-   ;;;;;; Module initialization ;;;;;;
-
-   ;; Initial module setup and first function
-   [--> (((module f_1 f_2 ...) e) κ scope table)
-        (() (module-stack e (f_2 ...) κ) scope (function-add table f_1))
-        function-add-init]
-
-   ;; 2-n functions
-   [--> (() (module-stack e (f_1 f_2 ...) κ) scope table)
-        (() (module-stack e (f_2 ...) κ) scope (function-add table f_1))
-        function-add]
-
-   ;; Execute the helper instruction
-   [--> (() (module-stack e () κ) scope table)
-        (e κ scope table)
-        function-done]
-
-   ;;;;;; Primitive Evaluation ;;;;;;
+   ;;;;; LOAD PROGRAM
    
-   ;; Load instruction to stack
-   [--> ((t op e_1 e_2 ...) κ scope table)
-        (e_1 (prim t (op) (e_2 ...) κ) scope table)
-        load_op]
+   ;; Load module functions to the function store
+   [--> (((module f_1 f_2 ...) e ...) L s)
+        (((module f_2 ...) e ...) L (function-add s f_1))
+        init-load-function]
 
-   ;; If expression arguments remain, swap
-   [--> (v (prim t (v_1 ... op) (e_1 e_2 ...) κ) scope table)
-        (e_1 (prim t (v_1 ... v op) (e_2 ...) κ) scope table)
-        prim_swap]
+   ;; After done with the module, begin executing instructions
+   [--> (((module) e ...) L s)
+        ((e ...) L s)
+        init-load-done]
 
-   ;;;;;; Locals ;;;;;;
-   [--> ((get-local i) κ (hashmap (v ...) scope) table)
-        ((hashmap-get hashmap i) κ (hashmap (v ...) scope) table)
-        get_local]
-
-   ;;;;;; Call Evaluation ;;;;;;
+   ;;;;; GENERAL INSTRUCTION EVALUATION
    
-   ;; Move first arg to control
-   [--> ((call i e_1 e_2 ...) κ scope table)
-        (e_1 (call-stack i () (e_2 ...) κ) scope table)
-        call_args_init]
+   ;; All binary operators
+   [--> ((v_rest ... v_2 v_1 binop e ...) L s)
+        ((v_rest ... (delta binop v_1 v_2) e ...) L s)
+        binop]
 
-   ;; If haven't evaluated all args, evaluate next arg
-   [--> (v_1 (call-stack i (v_2 ...) (e_1 e_2 ...) κ) scope table)
-        (e_1 (call-stack i (v_2 ... v_1) (e_2 ...) κ) scope table)
-        call_args_next]
+   ;; Param
+   [--> ((v_rest ... v_1 (param i) e ...) L s)
+        ((v_rest ... e ...) (local-set L i v_1) s)
+        param]
 
-   ;; After evaluating all args, make the call
-   [--> (v_1 (call-stack i (v_2 ...) () κ) scope table)
-        ((function-get table i) (call-stack i (v_2 ... v_1) () κ) (mt-h () scope) table)
-        function_call]
+   ;; Get-local
+   [--> ((v_rest ... (get-local i) e ...) L s)
+        ((v_rest ... (local-get L i) e ...) L s)
+        get-local]
 
-   ;; Call with no args
-   [--> ((call i) κ scope table)
-        ((function-get table i) (call-stack i () () κ) (mt-h () scope) table)
-        function_call_no_args]
+   ;;;;; FUNCTION CALLS
+   
+   ;; Call
+   [--> ((v_rest ... (call i) e ...) L s)
+        ((v_rest ... (function-get s i)) (mt-local (e ...) L) s)
+        call]
 
-   ;;;;;; Functions ;;;;;;
+   ;; Function expansion
+   [--> ((v_rest ... (func i e ...)) L s)
+        ((v_rest ... e ...) L s)
+        call-expansion]
 
-   ;; Add call stack args to scope bindings
-   [--> ((func i fe_1 fe_2 ...) (call-stack i (v_1 v_2 ...) () κ) (hashmap (v_arg ...) scope) table)
-        ((func i fe_1 fe_2 ...) (call-stack i (v_2 ...) () κ) (hashmap (v_arg ... v_1) scope) table)
-        function_entry_args]
-
-   ;; When all call stack args are added, start evaluating function expressions
-   [--> ((func i fe_1 fe_2 ...) (call-stack i () () κ) scope table)
-        (fe_1 (function-expressions fe_2 ... κ) scope table)
-        function_entry_args_done]
-
-   ;; Discard empty function expression frame and scope
-   [--> (v (function-expressions κ) (hashmap (v_args ...) scope) table)
-        (v κ scope table)
-        function_frame_discard]
-
-   ;; If function expression is a param statement, add param value to hashmap and continue evaluating
-   [--> ((param i t) (function-expressions fe_1 fe_2 ... κ) (hashmap (v_1 v_2 ...) scope) table)
-        (fe_1 (function-expressions fe_2 ... κ) ((hashmap-add hashmap i v_1) (v_2 ...) scope) table)
-        param_add]
-        
-
-   ;;;;;; To be named section ;;;;;;
-   ;; δ
-   [--> (v (prim t (v_1 ... op) () κ) scope table)
-        ((delta op v_1 ... v) κ scope table)
-        δ]))
-
+   ;; Implicit return
+   [--> ((v_rest ...) (local (e ...) L) s)
+        ((v_rest ... e ...) L s)
+        return-implicit]
+  
+   ))
+   
+                      
 (define (load p)
   (cond
-    [(redex-match? WebAssembly init p) (term (,p mt-k mt-s mt-t))]
+    [(redex-match? WASM-eval init p) (term (,p mt-context (mt-s-func mt-s-table mt-s-mem)))]
     [else (raise "load: expected a valid WASM program")]))
 
 (define-syntax test-wasm
@@ -178,60 +169,71 @@
      (test p r
            #:step ->
            #:load load
-           #:final? (λ (x) (and (list? x) (redex-match? WebAssembly mt-k (second x))))
-           #:extract first
+           #:final? (λ (x) (and (list? x) (redex-match? WASM-eval (v) (first x))))
+           #:extract (λ (x) (first (first x)))
            #:trace b)]))
 
 
-#;(test-wasm (term (i32 add (i32 const 5) (i32 const 5)))
-             (term (i32 const 10)) #:trace #f)
+; Trivial binop
+(test-wasm (term ((module) (const 5) (const 2) add))
+           (term (const 7)) #:trace #f)
 
-#;(test-wasm (term (i32 add
-                        (i32 const 5)
-                        (i32 add
-                             (i32 const 3)
-                             (i32 const 4))))
-             (term (i32 const 12)) #:trace #t)
+; Sequential binop
+(test-wasm (term ((module)
+                  (const 7)
+                  (const 5)
+                  (const 2)
+                  add
+                  add))
+           (term (const 14)) #:trace #f)
 
-; Simple function call with one argument
+; Sequential binopo with values after add op
+(test-wasm (term ((module)
+                  (const 7)
+                  (const 5)
+                  add
+                  (const 2)
+                  add))
+           (term (const 14)) #:trace #f)
+
+; Trivial function call
 (test-wasm (term ((module
-                      (func testFunc
-                            (param $0 i32)
-                            (get-local $0)))
-                  (call testFunc (i32 const 5))))
-           (term (i32 const 5)) #:trace #f)
+                      (func test_func
+                            (const 5)))
+                  (call test_func)))
+           (term (const 5)) #:trace #f)
 
-; Function call with two arguments
+; Function call with one argument
 (test-wasm (term ((module
-                      (func testFunc
-                            (param $0 i32)
-                            (param $1 i32)
-                            (get-local $1)))
-                  (call testFunc (i32 const 5) (i32 const 13))))
-           (term (i32 const 13)) #:trace #f)
+                      (func test_func
+                            (param $0)
+                            (const 5)
+                            (get-local $0)
+                            add))
+                  (const 3)
+                  (call test_func)))
+           (term (const 8)) #:trace #f)
 
-; Slightly more complex function call which does math on the argument
+; Calls with multiple args etc
 (test-wasm (term ((module
-                      (func testFunc
-                            (param $0 i32)
-                            (i32 add
-                                 (get-local $0)
-                                 (i32 const 1)))
-                    (func testFunc2 (i32 const 10)))
-                  (call testFunc (i32 const 5))))
-           (term (i32 const 6)) #:trace #f)
-
-; Function which calls another function
-(test-wasm (term ((module
-                      (func testFunc
-                            (param $0 i32)
-                            (i32 add
-                                 (call testFunc2 (i32 const 5))
-                                 (get-local $0)))
-                    (func testFunc2
-                          (param $0 i32)
-                          (i32 add
-                               (get-local $0)
-                               (i32 const 15))))
-                  (call testFunc (i32 const 6))))
-           (term (i32 const 26)) #:trace #f)
+                      (func callee
+                            (param $0)
+                            (param $1)
+                            (get-local $0)
+                            (get-local $1)
+                            add)
+                    (func caller
+                          (param $0)
+                          (param $1)
+                          (const 5)
+                          (get-local $0)
+                          (const 3)
+                          add
+                          (call callee)
+                          (const 1)
+                          add))
+                  (const 1)
+                  (const 2)
+                  (call caller)))
+           (term (const 11)) #:trace #t)
+           
