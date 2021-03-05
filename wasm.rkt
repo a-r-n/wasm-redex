911f05c697a47d5e55d83594d8aa6c33f973052d
TOCHUKWU <tochukwu@TOCHUKWUs-MacBook-Pro-2.local>
2021-03-05 17:44:34 -0600

    fixed changes to table and added memory implementation

--- wasm.rkt
+++ wasm.rkt
@@ -27,7 +27,7 @@
 (define-language WASM
   (c ::= real)
   (st ::= string)
-  (e ::= binop relop (const c) (call i) (call-indirect) trap debug-inst
+  (e ::= binop relop (const c) (call i) call-indirect  store load trap debug-inst
      (get-local i) (set-local i) (local i) (param i)
      (block e ... end)
      (loop e ... end)
@@ -35,12 +35,11 @@
   (binop ::= add sub mul div)
   (relop ::= eq ne)
   (f ::= (func i e ...))
-  (mm ::= (ex ... memory c) )
+  (mm ::= (memory j) )
   (table-type ::= funcref anyfunc)
-  (tab ::= (table c table-type))
+  (tab ::= (table j table-type))
   (elem-i ::= (elem v i i ...))
   (ex ::= (export st i) )
-  ;(im ::= (import st x))
   (m ::= (module tab elem-i ...  mm  f ...))
   (v ::= (const c))
   (i ::= variable-not-otherwise-mentioned)
@@ -53,12 +52,13 @@
 
   (op ::= binop relop)
   (init ::= (m e ...))
-  (Σ-mem ::= mt-mem-store '())  
-  ;(elem-c ::=  v_offset)
+  (mem-list ::= () (j st mem-list))
+  (mem-store ::=  st)
+  (s-mem ::= mt-s-mem (j mem-list))
   (s ::= (s-func s-table s-mem))
   (s-func ::= mt-s-func (i f s-func))
-  (s-mem ::= mt-s-mem (c_memid v_index v Σ-mem s-mem))
-  (s-table ::= mt-s-table  (table-type c_size v_offset v_index i s-table))
+  (table-elem ::= mt-table-elem (v_index i table-elem))
+  (s-table ::= mt-s-table  (table-type j_size  table-elem))
   (L ::= mt-context (labels locals (e ...) L))
   ;; the signature of labels is (loop-instrs instrs-after-block labels)
   (labels ::= mt-labels ((e ...) (e ...) labels))
@@ -85,35 +85,21 @@
 ;; add table
 (define-metafunction WASM-eval
   table-add : s tab -> s
-  [(table-add (s-func s-table s-mem) (table c_1 table-type )) (s-func (table-type c_1 (const 0) (const 0) $nopointer s-table) s-mem)])
-
-;;update table offset
-(define-metafunction WASM-eval
-  table-update-offset : s-func s-mem s-table v -> s
-  [(table-update-offset s-func s-mem (table-type c_size v_offset v_index i_1 s-table)  v_new)
-   (s-func  (table-type c_size v_new v_index i_1 mt-s-table) s-mem)])
-
-;;set table offset
-(define-metafunction WASM-eval
-  table-set-offset : s v -> s
-  [(table-set-offset (s-func s-table s-mem)  v_offset) (table-update-offset s-func s-mem s-table v_offset)])
-
+  [(table-add (s-func s-table s-mem) (table j_1 table-type )) (s-func (table-type j_1 ((const 0) $nopointer mt-table-elem)) s-mem)])
 ;;Add-update table 
 (define-metafunction WASM-eval
   table-add-update-function : s-func s-mem s-table i -> s
-  [(table-add-update-function s-func s-mem (table-type c_size v_offset v_index i_func s-table)  i_funcN)
-   ,(if (equal? (term i_func) (term $nopointer))
+  [(table-add-update-function s-func s-mem (table-type j_size (v_index i_1 table-elem))  i_funcN)
+   ,(if (equal? (term i_1) (term $nopointer))
         ;;update the table with the function and build up the store
         (term ( s-func
-                (table-type c_size v_offset v_index i_funcN mt-s-table)
+                (table-type j_size (v_index i_funcN table-elem))
                 s-mem))
         ;;add the function 
-        (if (< (term (extract-const-value v_offset)) (- (term c_size) 1))
+        (if (< (term (get-const v_index)) (- (term j_size) 1))
             ;;add the function to the table and build up the store
             (term ( s-func
-                    (table-type c_size  (const ,(+ (term (extract-const-value v_offset)) 1))
-                                (const ,(+ (term (extract-const-value v_index)) 1)) i_funcN
-                                (table-type c_size v_offset v_index i_func s-table))
+                    (table-type j_size ((const ,(+ (term (get-const v_index)) 1)) i_funcN (v_index i_1 table-elem)))
                     s-mem))
 
             ;;throw exception
@@ -121,8 +107,8 @@
 
 ;;helper meta-function to extract number from  (const c) value
 (define-metafunction WASM-eval
-  extract-const-value : v -> c
-  [(extract-const-value (const c_1)) c_1])
+  get-const : v -> c
+  [(get-const (const c_1)) c_1])
 
 
 ;;set table add function
@@ -139,20 +125,111 @@
 
 (define-metafunction WASM-eval
   search-table : s s-table v -> f
-  [(search-table s (table-type c_size v_offset v_index i s-table) v_1)
+  [(search-table s (table-type j_size (v_index i_1 table-elem)) v_1)
    ;;use the index passed to fetch the function
-   ,(if (= (term (extract-const-value v_1))  (term (extract-const-value v_index)) )
-        (term (function-get s i))
-        (term (search-table s s-table v_1)))]
-    ;;throw exception, if its not found
+   ,(if (= (term (get-const v_1))  (term (get-const v_index)) )
+        (term (function-get s i_1))
+        (term (search-table s (table-type j_size table-elem) v_1)))]
+  ;;throw exception, if its not found
   [(search-table s mt-s-table v_1) (func DEBUG-BAD (debug "Function not found in Table" v_1))])
   
 
-;; Get and add Memory
 (define-metafunction WASM-eval
   memory-add : s mm -> s
-  [(memory-add (s-func s-table s-mem) (ex ... memory c_1)) (s-func s-table (c_1 (const 0) (const 0) mt-mem-store s-mem))])
+  [(memory-add (s-func s-table s-mem) (memory j_1)) (s-func s-table ( ,(* (term j_1) 64) (init-mem 0 s-mem () ,(make-string 32 #\0))))])
 
+(define-metafunction WASM-eval
+  init-mem : j s-mem mem-list st -> mem-list
+  [(init-mem j s-mem mem-list st)
+   ,(if (and (equal? (term s-mem) (term mt-s-mem)) (not (equal? (term st) "")))
+            (term (init-mem
+               ,(+ (term j) 1)
+               s-mem
+               (j ,(substring (term st) (- (string-length (term st)) 8) (string-length (term st))) mem-list)
+               ,(substring (term st) 0 (- (string-length (term st)) 8))))
+            
+            (term mem-list))])
+
+
+(define-metafunction WASM-eval
+  memory_store : s v_1 v_2  -> s
+  [(memory_store (s-func s-table (j mem-list)) v_1 v_2)
+   (store-in-memory s-func s-table j mem-list v_1 v_2 )])
+
+(define-metafunction WASM-eval
+  store-in-memory : s-func s-table  j mem-list v_1 v_2 -> s
+  [(store-in-memory  s-func s-table j_size (j_index st mem-list) (const c_offset) (const c_value) )
+   ,(if(= (term j_index) (term c_offset))
+      (term
+       (store-in-memory-helper s-func s-table j_size mem-list
+                                           ,(string-append
+                                            (make-string (- 32 (string-length(number->string (term c_value) 2))) #\0)
+                                            (number->string (term c_value) 2))))
+
+      (term (store-in-memory  s-func s-table j_size mem-list (const c_offset) (const c_value))))])
+
+(define-metafunction WASM-eval
+  store-in-memory-helper : s-func s-table  j mem-list st -> s
+  [(store-in-memory-helper s-func s-table j_size (j_index st_1 mem-list_1) st )
+   ,(if ( <  (+ (term j_index) 1) (* (term j_size) 1024))
+     (if(not (equal? (term st) ""))
+      (term
+       (store-in-memory-helper
+        s-func
+        s-table
+        j_size
+        (,(+ (term j_index) 1) ,(substring (term st) (- (string-length (term st)) 8) (string-length (term st))) (j_index st_1 mem-list_1))
+      ,(substring (term st) 0 (- (string-length (term st)) 8))))
+      
+      (term (s-func s-table (j_size (j_index st_1 mem-list_1)))) )
+     
+     (term (func DEBUG-BAD (debug "Memory size overflow" ,(* (+ (term j_index) 1) 8))))
+     )]
+[(store-in-memory-helper s-func s-table j_size () st )
+ ,(if(not (equal? (term st) ""))
+     (term
+       (store-in-memory-helper  
+        s-func
+        s-table
+        j_size
+        (0 ,(substring (term st) (- (string-length (term st)) 8) (string-length (term st))) ())
+        ,(substring (term st) 0 (- (string-length (term st)) 8))))
+
+     (term (s-func s-table (j ()))))])
+
+
+;;load from memory
+(define-metafunction WASM-eval
+  load-from-memory : s v  -> v
+  [(load-from-memory (s-func s-table (j mem-list))  v_offset) (read-from-memory mem-list v_offset)])
+
+
+(define-metafunction WASM-eval
+  read-from-memory : mem-list v -> v
+  [(read-from-memory  (j_index st mem-list) (const c))
+   ,(if (<= (+ (term c) 3) (term j_index))
+        (if (= (term j_index)  (+ (term c) 3) )
+        (term (read-from-memory-helper (j_index st mem-list) c ""))
+
+        (term (read-from-memory mem-list (const c))))
+
+        (term (func DEBUG-BAD (debug "Offset out of range" (const c)))))])
+
+(define-metafunction WASM-eval
+  read-from-memory-helper : mem-list j st -> v
+  [(read-from-memory-helper (j_index st_1 mem-list) j_1 st)
+   ,(if (= (term j_index)  (term j_1))
+        (term (const ,(string->number (string-append (term st) (term st_1)) 2 )))
+
+        (term (read-from-memory-helper mem-list j_1 ,(string-append (term st) (term st_1)))))])
+
+
+;;helper meta-function to build memory offset track
+(define-metafunction WASM-eval
+  make-memory-offsets : v mem-offsets -> mem-offsets
+  [(make-memory-offsets v_1 (j_start j_end mem-offsets))
+   (j_end ,(+ (string-length (number->string (term (extract-const-value v_1)) 2)) (term j_end)) (j_start j_end mem-offsets))]
+  [(make-memory-offsets v_1 mt-offset) (0  ,(string-length (number->string (term (extract-const-value v_1)) 2)) mt-offset)])
 
 ;; Get and add functions
 
@@ -228,7 +305,7 @@
 
    ;; update module table in store
    [--> (((module (elem v i_1 i_2 ...) mm_1  f_1 ...) e ...) L s)
-        (((module  i_1 i_2 ... mm_1 f_1 ...) e ...) L (table-set-offset s v))
+        (((module  i_1 i_2 ... mm_1 f_1 ...) e ...) L s)
         init-update-table]
 
    ;;table manipulation
@@ -307,11 +384,26 @@
    ;;;;;;; FUNCTION CALL FROM TABLE
    
    ;; Call Indirect
-   [--> ((v_rest ... v_1 (call-indirect) e ...) L s)
+   [--> ((v_rest ... v_1 call-indirect e ...) L s)
         ((v_rest ... (fetch-from-table s v_1))(mt-labels mt-locals (e ...)  L) s)
         call-indirect]
+
+   ;;;;;;; MEMORY STORE AND LOAD
+
+   ;;STORE TO MEMORY
+   
+   [--> ((v_rest ... v_1 v_2 store e ...) L s)
+        ((v_rest ... e ...)L (memory_store s v_1 v_2 ))
+        store-memory]
+   ;
+   [--> ((v_rest ... v_1 load e ...) L s)
+        ((v_rest ... (load-from-memory s v_1))(mt-labels mt-locals (e ...)  L) s)
+        load-memory]
+
    
 
+  
+ 
    ;;;;; BRANCHING
 
    ;; Block
@@ -603,13 +695,13 @@
 
 ;;simple instantiation test
 (test-wasm (term ((module
-                        (table 2 anyfunc)
-                      (elem (const 0) $f1 $f2)
-                      (memory  1 ))
-                    (const 5)
-                    (const 2)
-                    add))
-             (term (const 7)) #:trace #f)
+                      (table 2 anyfunc)
+                    (elem (const 0) $f1 $f2)
+                    (memory  1 ))
+                  (const 5)
+                  (const 2)
+                  add))
+           (term (const 7)) #:trace #f)
 
 ;; simple call-indirect test
 (test-wasm (term ((module
@@ -624,40 +716,197 @@
                           add
                           ))
                   (const 0)
-                  (call-indirect)))
-           (term (const 2)) #:trace #t)
+                  call-indirect))
+           (term (const 2)) #:trace #f)
 
 ;;complex call-indirect test
 (test-wasm (term ((module
-                        (table 2 anyfunc)
-                      (elem (const 0) my_func1 my_func2)
-                      (memory  1 )
+                      (table 2 anyfunc)
+                    (elem (const 0) my_func1 my_func2)
+                    (memory  1 )
 
-                  (func my_func1
+                    (func my_func1
                           (const 5)
                           (const 2)
                           add)
-                  (func my_func2
+                    (func my_func2
                           (const 5)
                           (const 2)
                           add
                           
                           (const 0)
-                          (call-indirect)
+                          call-indirect
                           add))
-                 (call my_func2)))
-             (term (const 14)) #:trace #f)
+                  (call my_func2)))
+           (term (const 14)) #:trace #f)
+
+
+;;testing memory store
+(test-wasm (term ((module
+                      (table 2 anyfunc)
+                    (elem (const 0) $f1 $f2)
+                    (memory  1 ))
+                  
+                  (const 0)
+                  (const 30)
+                  store
+                  (const 5)
+                  (const 2)
+                  add))
+           (term (const 7)) #:trace #f)
+
 
 
 (test-wasm (term ((module
-                      (table 0 anyfunc)
-                    (memory  1 ) 
-                    (func my_func
-                          (block
-                           (const 1)
-                           end)
-                          (const 1)
+                      (table 2 anyfunc)
+                    (elem (const 0) $f1 $f2)
+                    (memory  1 ))
+                  
+                  (const 0)
+                  (const 30)
+                  store
+                   (const 1)
+                  (const 120)
+                  store
+                  (const 5)
+                  (const 2)
+                  add))
+           (term (const 7)) #:trace #f)
+
+(test-wasm (term ((module
+                      (table 2 anyfunc)
+                    (elem (const 0) $f1 $f2)
+                    (memory  1 ))
+                  
+                  (const 0)
+                  (const 30)
+                  store
+                   (const 1)
+                  (const 800)
+                  store
+                  (const 5)
+                  (const 2)
+                  add))
+           (term (const 7)) #:trace #f)
+
+(test-wasm (term ((module
+                      (table 2 anyfunc)
+                    (elem (const 0) $f1 $f2)
+                    (memory  1 ))
+                  
+                  (const 0)
+                  (const 800)
+                  store
+                   (const 1)
+                  (const 800)
+                  store
+                  (const 5)
+                  (const 2)
+                  add))
+           (term (const 7)) #:trace #f)
+
+(test-wasm (term ((module
+                      (table 2 anyfunc)
+                    (elem (const 0) my_func1 my_func2)
+                    (memory  1 )
+
+                    (func my_func1
+                          (const 5)
+                          (const 2)
+                          add)
+                    (func my_func2
+                          (const 5)
+                          (const 2)
                           add
-                          ))
-                  (call my_func)))
-           (term (const 2)) #:trace #f)
+                          (const 0)
+                          (const 244)
+                          store
+                          (const 1)
+                          (const 210)
+                          store
+                          (const 2)
+                          (const 120)
+                          store
+                          (const 0)
+                          call-indirect
+                          add))
+                  (call my_func2)))
+           (term (const 14)) #:trace #f)
+
+(test-wasm (term ((module
+                      (table 2 anyfunc)
+                    (elem (const 0) $f1 $f2)
+                    (memory  1 ))
+                  
+                  (const 0)
+                  (const 30)
+                  store
+                  (const 0)
+                  load
+                  (const 2)
+                  add))
+           (term (const 32)) #:trace #f)
+
+(test-wasm (term ((module
+                      (table 2 anyfunc)
+                    (elem (const 0) $f1 $f2)
+                    (memory  1 ))
+                  
+                  (const 0)
+                  (const 800)
+                  store
+                   (const 1)
+                  (const 800)
+                  store
+                  (const 0)
+                  load))
+           (term (const 204832)) #:trace #f)
+
+(test-wasm (term ((module
+                      (table 2 anyfunc)
+                    (elem (const 0) $f1 $f2)
+                    (memory  1 ))
+                  
+                  (const 100)
+                  (const 800)
+                  store
+                   ;(const 1)
+                  ;(const 800)
+                 ; store
+                 ; (const 1)
+                  ;load
+                  ))
+           (term (const 800)) #:trace #t)
+
+
+
+(test-wasm (term ((module
+                      (table 2 anyfunc)
+                    (elem (const 0) my_func1 my_func2)
+                    (memory  1 )
+
+                    (func my_func1
+                          (const 5)
+                          (const 2)
+                          add)
+                    (func my_func2
+                          (const 5)
+                          (const 2)
+                          add
+                          (const 0)
+                          (const 244)
+                          store
+                          (const 1)
+                          (const 210)
+                          store
+                          (const 2)
+                          (const 120)
+                          store
+                          (const 0)
+                          load
+                          add))
+                  (call my_func2)))
+           (term (const 7918331)) #:trace #f)
+
+;(string-append (string-append (make-string (- 8 (string-length (number->string 2 2))) #\0) (number->string 2 2))
+;                 (make-string (- 32 (string-length (string-append (make-string (- 8 (string-length (number->string 2 2))) #\0) (number->string 2 2)))) #\0))
